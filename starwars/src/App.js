63c391d2865672732bc43b6d2f033a7781859e5b
Kristin Barr <kristinbarr@gmail.com>
2019-07-19 12:01:00 -0400

    initial commit and data reference

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [ char, setChar ] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.co/api/people/')
+      .then((res) => {
+        setChar(res)
+        console.log(res)
+      })
+      .catch((error) => {
+        console.log('something went wrong!', error)
+      })
+  }, [])
+
+  // Try to think through what state you'll need for this app before starting. Then build out the state properties here.
+  // name, birth year, species, homeworld, maybe films, maybe pull picture from wiki with name
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a side effect in a component, you want to think about which state and/or props it should sync up with, if any.
 
   return (
     <div className="App">
