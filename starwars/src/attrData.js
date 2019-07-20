4666cb49b5ff78bd60ff6595d4b8b85edfbb09c6
Kristin Barr <kristinbarr@gmail.com>
2019-07-20 03:52:27 -0400

    added species, photo data seed

---
+++ starwars/src/attrData.js
@@ -0,0 +1,39 @@
+export const charSpecies = {
+  1: 'Human',
+  2: 'Droid',
+  3: 'Wookiee',
+  4: 'Rodian',
+  5: 'Hutt',
+  6: "Yoda's species",
+  7: 'Trandoshan',
+  8: 'Mon Calamari',
+  9: 'Ewok',
+  10: 'Sullustan',
+  11: 'Neimodian',
+  12: 'Gungan'
+}
+
+export const photos = {
+  'Anakin Skywalker':
+    'https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/',
+  'Obi-Wan Kenobi':
+    'https://vignette.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg/',
+  'R2-D2':
+    'https://vignette.wikia.nocookie.net/starwars/images/e/eb/ArtooTFA2-Fathead.png/',
+  'Luke Skywalker':
+    'https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg',
+  'C-3PO':
+    'https://vignette.wikia.nocookie.net/starwars/images/3/3f/C-3PO_TLJ_Card_Trader_Award_Card.png/',
+  'Darth Vader':
+    'https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/',
+  'Leia Organa':
+    'https://vignette.wikia.nocookie.net/starwars/images/f/fc/Leia_Organa_TLJ.png/',
+  'Owen Lars':
+    'https://vignette.wikia.nocookie.net/starwars/images/9/91/OwenLarsHS-SWE.jpg/',
+  'Beru Whitesun lars':
+    'https://vignette.wikia.nocookie.net/starwars/images/7/76/Beru_headshot2.jpg/',
+  'R5-D4':
+    'https://vignette.wikia.nocookie.net/starwars/images/c/cb/R5-D4_Sideshow.png/',
+  'Biggs Darklighter':
+    'https://vignette.wikia.nocookie.net/starwars/images/0/00/BiggsHS-ANH.png/'
+}
