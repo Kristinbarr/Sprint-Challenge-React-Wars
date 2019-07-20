191fd64d3b66d1616147d1ba52acc7da1f0d6b52
Kristin Barr <kristinbarr@gmail.com>
2019-07-20 03:56:56 -0400

    removed unused code

--- starwars/src/components/CharGrid/CharGrid.js
+++ starwars/src/components/CharGrid/CharGrid.js
@@ -1,18 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Card from '../Card/Card'
 import data from '../../swapiData'
-import './CharGrid.css'
 
-const CharGrid = (props) => {
-  const charArr = props.charData.results
-  // console.log('chargrid', charArr, Array.isArray(charArr))
-  // console.log('data', data.results)
-  // console.log('APIarr',charArr)
+const CharGrid = () => {
+  const charArr = data.results
 
   return (
     <div className='char-grid'>
-      {data.results.map(char => <Card char={char} key={char.created} />
-      )}
+      {charArr.map(char => (
+        <Card char={char} key={char.created} />
+      ))}
     </div>
   )
 }
